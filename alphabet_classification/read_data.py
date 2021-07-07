380ca25f9f93785932c774239ecbe7cfb3e453fa
Kim, GiBeom <gibum1228@gmail.com>
2021-07-07 18:51:14 +0900

    커스텀 데이터셋 생성 (갯수, w, h, ch)

--- alphabet_classification/read_data.py
+++ alphabet_classification/read_data.py
@@ -1,4 +1,5 @@
 import csv
+import matplotlib
 import numpy as np
 import pandas as pd
 import seaborn as sns
@@ -6,8 +7,15 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-def get_data(filename):
-    with open(filename) as training_file:
+from PIL import Image
+import matplotlib.pyplot as plt
+import torch
+from torch.utils.data import Dataset, DataLoader
+import torchvision
+from torchvision import transforms
+
+def get_data_for_csv(path):
+    with open(path) as training_file:
         training_reader = csv.reader(training_file, delimiter=',')
         image = []
         labels = []
@@ -27,11 +35,27 @@ def get_data(filename):
 
     return images, labels
 
-# root = "/home/gibeom/dataset/asl_mnist"
-# train_images, train_labels = get_data(root + "/sign_mnist_train.csv")
-# test_images, test_labels = get_data(root + "/sign_mnist_test.csv")
-#
-# print("Total Training images", train_images.shape)
-# print("Total Training labels", train_labels.shape)
-# print("Total Testing images", test_images.shape)
-# print("Total Testing labels", test_labels.shape)
+def get_data_for_dataloader(path):
+    # 데이터 전처리
+    trans = transforms.Compose([transforms.Resize((32, 32)),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+    custom_dataset = torchvision.datasets.ImageFolder(root=path,
+                                                      transform=trans)
+
+    data_loader = DataLoader(custom_dataset,
+                             batch_size=2000,
+                             shuffle=False,
+                             num_workers=10)
+
+    data_iter = iter(data_loader)
+    images, labels = np.empty((0, 32, 32, 3)), np.empty((0,))
+    try:
+        for _ in range(len(data_iter)):
+            i, l = data_iter.next()
+            images = np.append(images, i.numpy().reshape(-1, 32, 32, 3), axis=0)
+            labels = np.append(labels, l.numpy(), axis=0)
+    except Exception:
+        print()
+
+    return images, labels
