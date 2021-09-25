51ea0be838eced472b9129e864b42cfb7235c2b9
Kim, GiBeom <gibum1228@gmail.com>
2021-09-25 19:23:48 +0900

    update

--- alphabet_classification/evaluate.py
+++ alphabet_classification/evaluate.py
@@ -8,8 +8,12 @@ from sklearn.model_selection import train_test_split
 import read_data
 
 if __name__ == "__main__":
-    model = tf.keras.models.load_model('data/test_sgd.h5')
-    labels = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+    model_case1 = tf.keras.models.load_model('data/lenet5_alphabet_1000.h5')
+    model_case2 = tf.keras.models.load_model('data/lenet5_digit_1000.h5')
+    model_case3 = tf.keras.models.load_model('data/lenet5_recognition_1000_v2_acc99.h5')
+    model = tf.keras.models.load_model('data/lenet5_recognition_1000_v2_acc99.h5')
+    labels = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+              'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
               'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
     # read data
@@ -50,11 +54,19 @@ if __name__ == "__main__":
     train_labels_digit = np.concatenate([train_labels_1, train_labels_2])
     test_images_digit = np.concatenate([test_images_1, test_images_2])
     test_labels_digit = np.concatenate([test_labels_1, test_labels_2])
+    train_labels_alphabet = train_labels_alphabet + 10
+    test_labels_alphabet = test_labels_alphabet + 10
 
     train_images = np.concatenate([train_images_alphabet, train_images_digit])
     train_labels = np.concatenate([train_labels_alphabet, train_labels_digit])
     test_images = np.concatenate([test_images_alphabet, test_images_digit])
     test_labels = np.concatenate([test_labels_alphabet, test_labels_digit])
+
+    # train accuracy 구하기
+    print(model_case1.evaluate(train_images_alphabet, train_labels_alphabet))
+    print(model_case2.evaluate(train_images_digit, train_labels_digit))
+    print(model_case3.evaluate(train_images, train_labels))
+    exit()
 
     # predict
     pred = model.predict(test_images)
@@ -64,8 +76,8 @@ if __name__ == "__main__":
     test_labels = test_labels.astype('int64')
     # y_test_word = [labels[i] for i in test_labels]
     # pred_word = [labels[i] for i in pred]
-    y_test_word = [i for i in test_labels]
-    pred_word = [i for i in pred]
+    y_test_word = [labels[i] for i in test_labels]
+    pred_word = [labels[i] for i in pred]
     # print table
     # print(classification_report(y_test_word, pred_word))
     #
@@ -77,7 +89,7 @@ if __name__ == "__main__":
     cf_matrix = confusion_matrix(y_test_word, pred_word, normalize='true')
     plt.figure(figsize=(30, 15))
     sns.heatmap(cf_matrix, annot=True, xticklabels=sorted(set(y_test_word)), yticklabels=sorted(set(y_test_word)), cbar=False)
-    plt.title("Digit(SGD) Confusion Matrix\n", fontsize=25)
+    plt.title("CASE 3 Confusion Matrix\n", fontsize=25)
     plt.xlabel("Predict", fontsize=20)
     plt.ylabel("True", fontsize=20)
     plt.xticks(fontsize=15)
